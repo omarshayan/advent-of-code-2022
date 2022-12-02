e2ad916d043c9181d350774ebf53c186f800e179
omie <omarshayanriaz@gmail.com>
2022-12-02 17:37:28 -0500

    day2

---
+++ day2/star1.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <map>
+
+
+using namespace std;
+
+int convertRPSCode(string code)
+{
+   map<string, int> map; 
+   map.insert(pair<string, int>("A", 0));
+   map.insert(pair<string, int>("B", 1));
+   map.insert(pair<string, int>("C", 2));
+   map.insert(pair<string, int>("X", 0));
+   map.insert(pair<string, int>("Y", 1));
+   map.insert(pair<string, int>("Z", 2));
+
+  return map[code];
+}
+
+int main()
+{
+    vector<int> opponent_moves;
+    vector<int> my_moves;
+    int elf_idx = 0;
+    // read input    
+    ifstream reader("input.txt");
+    while(!reader.eof())
+    {
+        string rps_round;
+        getline(reader, rps_round);
+        cout << rps_round << endl;
+        if (rps_round != "") {
+            opponent_moves.push_back(convertRPSCode(rps_round.substr(0,1)));
+            my_moves.push_back(convertRPSCode(rps_round.substr(2,1)));
+        }
+    }
+    
+        cout << "okay"  << endl;
+    int score = 0;
+    for(int i=0;i<my_moves.size();i++)
+    {
+        cout << my_moves[i] << " vs " << opponent_moves[i];
+        score += my_moves[i] + 1;
+        if (my_moves[i] == (opponent_moves[i] + 1) % 3) { score += 6; cout << " win!"; }
+        if (opponent_moves[i] == (my_moves[i] + 1) % 3) { score += 0; }
+        if (my_moves[i] == opponent_moves[i]) { score += 3; cout << " draw.";}
+        cout << endl;
+    }
+
+    cout << "final score: " << score << endl;
+    return 0;
+}
